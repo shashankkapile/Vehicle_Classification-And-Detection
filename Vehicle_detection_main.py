6ac3a2764521c6fcd68095bdd132887b178f76b8
AlfaCodeFlow <prashantsingh2727@gmail.com>
2019-04-02 17:05:46 +0530

    Update Vehicle_detection_main.py

--- Vehicle_detection_main.py
+++ Vehicle_detection_main.py
@@ -1,95 +1,50 @@
 import cv2
 import numpy as np
-import vehicles
 import time
-import myMorpho as mp
-from scipy import ndimage, misc
 
-cnt_up=0
-cnt_down=0
-
-
-cap=cv2.VideoCapture("video.m4v")
-
-
-
-
-#Get width and height of video
-k=1
-w=0
-h=0
-while k<2:
-    ret,frame=cap.read()
-    frame = misc.imresize(frame, (320, 640))
-    g=tuple(frame.shape)
-    w=g[1]
-    h=g[0]
-    k=k+1
-
-
-frameArea=h*w
-areaTH=frameArea/350
-
-
-line_up=int(3.5*(h/5))
-line_down=int(3*(h/5))
-
-up_limit=int(2.5*(h/5))
-down_limit=int(4*(h/5))
-
-
-line_down_color=(0,255,)
-line_up_color=(0,255,0)
-pt1 =  [0, line_down]
-pt2 =  [w, line_down]
-pts_L1 = np.array([pt1,pt2], np.int32)
-pts_L1 = pts_L1.reshape((-1,1,2))
-pt3 =  [0, line_up]
-pt4 =  [w, line_up]
-pts_L2 = np.array([pt3,pt4], np.int32)
-pts_L2 = pts_L2.reshape((-1,1,2))
-
-pt5 =  [0, up_limit]
-pt6 =  [w, up_limit]
-pts_L3 = np.array([pt5,pt6], np.int32)
-pts_L3 = pts_L3.reshape((-1,1,2))
-pt7 =  [0, down_limit]
-pt8 =  [w, down_limit]
-pts_L4 = np.array([pt7,pt8], np.int32)
-pts_L4 = pts_L4.reshape((-1,1,2))
-
-#Background Subtractor
-fgbg=cv2.createBackgroundSubtractorMOG2(detectShadows=True)
-
-
+cap=cv2.VideoCapture("video.mp4")
+fgbg=cv2.createBackgroundSubtractorMOG2(detectShadows=False,history=200,varThreshold = 90)
+kernalOp = np.ones((3,3),np.uint8)
+kernalOp2 = np.ones((5,5),np.uint8)
+kernalCl = np.ones((11,11),np.uint8)
 font = cv2.FONT_HERSHEY_SIMPLEX
 cars = []
 max_p_age = 5
 pid = 1
+cnt_up=0
+cnt_down=0
+
+
+line_up=400
+line_down=250
+
+up_limit=230
+down_limit=int(4.5*(500/5))
 
 while(cap.isOpened()):
     ret,frame=cap.read()
-    frame = misc.imresize(frame, (320, 640))
+    frame=cv2.resize(frame,(900,500))
     for i in cars:
         i.age_one()
     fgmask=fgbg.apply(frame)
 
     if ret==True:
+        ret,imBin=cv2.threshold(fgmask,200,255,cv2.THRESH_BINARY)
+        mask = cv2.morphologyEx(imBin, cv2.MORPH_OPEN, kernalOp)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernalCl)
 
-        ret,BinImage=cv2.threshold(fgmask,200,255,cv2.THRESH_BINARY)
-        mask=mp.mymorpho(BinImage)
 
-        #Find Contours
         (countours0,hierarchy)=cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
         for cnt in countours0:
             area=cv2.contourArea(cnt)
+            print(area)
+            if area>300:
 
-            if area>areaTH:
-                #Tracking by centroid of object
                 m=cv2.moments(cnt)
                 cx=int(m['m10']/m['m00'])
                 cy=int(m['m01']/m['m00'])
                 x,y,w,h=cv2.boundingRect(cnt)
+
 
                 new=True
                 if cy in range(up_limit,down_limit):
@@ -118,30 +73,45 @@ while(cap.isOpened()):
                     if new==True:
                         p=vehicles.Car(pid,cx,cy,max_p_age)
                         cars.append(p)
-                        pid+=1
+                        pid+1
+                cv2.circle(frame, (cx, cy), 2, (0, 0, 255), -1)
 
-                cv2.circle(frame,(cx,cy),5,(0,0,255),-1)
-                img=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+
+
+                img=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+
+
+
+
 
         for i in cars:
-            cv2.putText(frame, str(i.getId()), (i.getX(), i.getY()), font, 0.2, (255,255), 1, cv2.LINE_AA)
+            cv2.putText(frame, str(i.getId()), (i.getX(), i.getY()), font, 0.3, i.getRGB(), 1, cv2.LINE_AA)
+            if line_down+20<= i.getY() <= line_up-20:
+               a = (h + (.74*w)- 147)
 
-
+               if a >= 0:
+                     cv2.putText(frame, "Truck", (i.getX(), i.getY()), font, 1, (0,0,255), 2, cv2.LINE_AA)
+               else:
+                     cv2.putText(frame, "car", (i.getX(), i.getY()), font, 1, (0,0,255), 2, cv2.LINE_AA)
 
 
         str_up='UP: '+str(cnt_up)
         str_down='DOWN: '+str(cnt_down)
-        frame=cv2.polylines(frame,[pts_L1],False,line_down_color,thickness=3)
-        frame=cv2.polylines(frame,[pts_L2],False,line_up_color,thickness=3)
-        frame=cv2.polylines(frame,[pts_L3],False,(255,255,255),thickness=1)
-        frame=cv2.polylines(frame,[pts_L4],False,(255,255,255),thickness=1)
+        frame=cv2.line(frame,(0,line_up),(900,line_up),(0,0,255),3,8)
+        frame=cv2.line(frame,(0,up_limit),(900,up_limit),(0,0,0),1,8)
 
-        cv2.putText(frame, str_up, (10, 40), font, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
+        frame=cv2.line(frame,(0,down_limit),(900,down_limit),(255,255,0),1,8)
+        frame = cv2.line(frame, (0, line_down), (900, line_down), (255, 0,0), 3, 8)
 
-        cv2.putText(frame, str_down, (10, 90), font, 0.5, (0,0 , 255), 2, cv2.LINE_AA)
+
+
+
+        cv2.putText(frame, str_up, (10, 40), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+        cv2.putText(frame, str_down, (10, 90), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
         cv2.imshow('Frame',frame)
 
-        if cv2.waitKey(1)&0xff==ord('s'):
+        if cv2.waitKey(1)&0xff==ord('q'):
             break
 
     else:
@@ -149,11 +119,3 @@ while(cap.isOpened()):
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
